bb9c541d79fa870e361b89c32aece096438152e9
papafay <153556700+papafay@users.noreply.github.com>
2025-11-04 14:41:55 +0100

    Update main.py

--- P1/P1C5/énoncé/main.py
+++ P1/P1C5/énoncé/main.py
@@ -1,1 +1,9 @@
-# Écrivez votre code ici !
+nom=("papa samba faye")
+age=22
+est_etudiant=true 
+taille=1.86
+print(f"mon nom est {nom} j'ai{age} je fais {taille} cm et je suis {est_etudiant}")
+type(nom)
+type(age)
+type(taille)
+type(est_etudiant)# Écrivez votre code ici !
